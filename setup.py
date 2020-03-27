044935696609e3ec631c134055d8884e702c3104
Amy Summers <amyfoxxie@gmail.com>
2020-03-27 14:29:39 +0000

    Updated setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="cerberus-list-schema-firepubes",
+    name="cerberus-list-schema",
     version="1.0",
     author="Amy Summers",
     author_email="amy@sakuradigital.co.uk",
@@ -13,7 +13,12 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Firepubes/cerberus-list-schema",
+    license='MIT',
     packages=setuptools.find_packages(),
+    install_requires=[
+        'cerberus',
+        'munch',
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
